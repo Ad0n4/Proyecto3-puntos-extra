a651c9f3a7ff49caf5262b4bfc20e5a27be59b36
Ad0n4 <adopal64@gmail.com>
2026-06-11 01:35:58 -0600

    si

--- src/design/result_selector.sv
+++ src/design/result_selector.sv
@@ -1,11 +1,11 @@
 `timescale 1ns/1ps
 
 module result_selector (
-    input  logic [5:0] quotient_i,
-    input  logic [3:0] remainder_i,
+    input  logic [6:0] quotient_i,
+    input  logic [4:0] remainder_i,
     input  logic       select_i,
 
-    output logic [5:0] selected_result_o
+    output logic [6:0] selected_result_o
 );
 
     assign selected_result_o = (select_i) ? {2'b00, remainder_i} : quotient_i;
